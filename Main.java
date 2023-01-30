027dc9d3f4edf0034fd2f0476b03593caae391fb
Rasmus Hedin <rashed@skola.goteborg.se>
2023-01-30 12:25:20 +0100

    plz again

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("PLEEASE!!!");
         System.out.println("pls");
+        System.out.println("Still working?");
     }
 }
