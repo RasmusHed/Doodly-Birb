5af35e7d56c44be1551ff5ff5480ffce7c899469
RasmusHed <rashed1110@skola.goteborg.se>
2023-02-06 18:06:50 +0100

    Making the jump more smooth

---
+++ core/src/com/mygdx/game/Birb.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+
+public class Birb {
+    private Texture birb;
+    private Vector3 posistion;
+    private Vector3 velocity;
+    private static final float GRAVITY = - 0.234f;
+    private static final float JUMP = 8;
+
+    public Birb (int x, int y) {
+        posistion = new Vector3(x, y, 0);
+        velocity = new Vector3(0, 0, 0);
+        birb = new Texture("birbIcon.png");
+    }
+
+    public void gravity() {
+        velocity.add(0, GRAVITY, 0);
+        posistion.add(0, velocity.y, 0);
+    }
+
+    public void jump() {
+        velocity.y = 15;
+        velocity.scl(0.4f);
+    }
+
+    public Texture getTexture() {
+        return birb;
+    }
+
+    public Vector3 getPosistion() {
+        return posistion;
+    }
+}
--- core/src/com/mygdx/game/JumpyBirb.java
+++ core/src/com/mygdx/game/JumpyBirb.java
@@ -3,53 +3,54 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class JumpyBirb extends ApplicationAdapter {
-
-	private Texture birbImage;
-	private Rectangle birb;
+	//private Texture birbImage;
+	//private Rectangle birb;
+	private Birb birb;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 
+
 	@Override
 	public void create () {
-		birbImage = new Texture(Gdx.files.internal("birbIcon.png"));
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		birb = new Rectangle();
-		birb.x = 400 / 2 - 64 / 2;
-		birb.y = 240;
-		birb.width = 64;
-		birb.height = 64;
-
+		birb = new Birb(150, 240);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
+
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(birbImage, birb.x, birb.y);
-		batch.end();
-		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) birb.y += 2500 * Gdx.graphics.getDeltaTime();
-		if (birb.y > 0){
-			birb.y -= 90 * Gdx.graphics.getDeltaTime();
+		if (birb.getPosistion().y > 5) {
+			birb.gravity();
 		}
-		if (birb.y > 440) {
-			birb.y = 440;
+		batch.draw(birb.getTexture(), birb.getPosistion().x, birb.getPosistion().y);
+		batch.end();
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && birb.getPosistion().y < 470) {
+			birb.jump();
 		}
 
 	}
-	
 	@Override
 	public void dispose () {
-		birbImage.dispose();
+
 	}
 }
+
+
