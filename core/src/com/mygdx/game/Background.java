61dabe9e48dde3120d016186b025e2e94c51b7be
RasmusHed <rashed1110@skola.goteborg.se>
2023-02-28 16:57:45 +0100

    Making the background move with camera

--- core/src/com/mygdx/game/Background.java
+++ core/src/com/mygdx/game/Background.java
@@ -2,15 +2,18 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class Background {
     private static Texture backgroundImage;
     private static SpriteBatch spriteBatch;
+    private Vector2 backgroundPosistion;
 
 
-    public Background(float x) {
+    public Background(float x, float y) {
         spriteBatch = new SpriteBatch();
         backgroundImage = new Texture("firstBackground.png");
+        backgroundPosistion = new Vector2(x,y);
     }
 
     public Texture getBackgroundImage() {
@@ -19,5 +22,13 @@ public class Background {
 
     public SpriteBatch getSpriteBatch() {
         return spriteBatch;
+    }
+
+    public Vector2 getBackgroundPosistion() {
+        return backgroundPosistion;
+    }
+
+    public void setBackgroundPosistion(float posistion) {
+        this.backgroundPosistion.x = posistion;
     }
 }
