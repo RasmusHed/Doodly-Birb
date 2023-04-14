a190689b379cbb5ac668787aa6562ae207987fd4
Knut <knutbsh@gmail.com>
2023-04-14 11:59:15 +0200

    make funny noise for jump

--- core/src/com/mygdx/game/sprites/Birb.java
+++ core/src/com/mygdx/game/sprites/Birb.java
@@ -2,10 +2,13 @@ package com.mygdx.game.sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.mygdx.game.Settings;
+
+import java.util.Random;
 
 
 public class Birb {
@@ -14,12 +17,23 @@ public class Birb {
     private Texture birb;
     private Vector3 posistion;
     private Vector3 velocity;
+    private Sound jumpSound;
+    private Sound jumpSound1;
+    private Sound jumpSound2;
+    private Sound jumpSound3;
+    private Sound jumpSound4;
+    private Random random;
 
 
     public Birb(int x, int y) {
         posistion = new Vector3(x, y, 0);
         velocity = new Vector3(10, 0, 0);
         birb = new Texture("birbIcon.png");
+        jumpSound= Gdx.audio.newSound(Gdx.files.internal("sound.wav"));
+        jumpSound1= Gdx.audio.newSound(Gdx.files.internal("sound(1).wav"));
+        jumpSound2= Gdx.audio.newSound(Gdx.files.internal("sound(2).wav"));
+        jumpSound3= Gdx.audio.newSound(Gdx.files.internal("sound(3).wav"));
+        jumpSound4= Gdx.audio.newSound(Gdx.files.internal("sound(4).wav"));
 
         setHitBox();
     }
@@ -63,7 +77,20 @@ public class Birb {
     }
 
     public void jump() {
+        random = new Random();
+        int randomSound = random.nextInt(4);
         if (Gdx.input.justTouched() || Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && getPosistion().y < 470) {
+            if (randomSound == 0){
+                jumpSound.play();
+            } else if (randomSound == 1) {
+                jumpSound1.play();
+            } else if (randomSound == 2) {
+                jumpSound2.play();
+            } else if (randomSound == 3) {
+                jumpSound3.play();
+            } else if (randomSound == 4) {
+                jumpSound4.play();
+            }
             velocity.y = Settings.JUMP;
             velocity.scl(0.4f);
         }
